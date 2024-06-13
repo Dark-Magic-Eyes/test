b701310556e58812aa572be91c0c4784a5a94a30
VLong <vanlongtran2023@gmail.com>
2024-06-13 13:53:08 +0700

    Init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
+import React, { createContext } from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -15,8 +17,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+      <html lang="en">
+        <body className={inter.className}>
+          {/* <TonConnectUIProvider> */}
+            {children}
+          {/* </TonConnectUIProvider> */}
+        </body>
+      </html>
   );
 }
