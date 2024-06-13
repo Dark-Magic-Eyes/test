b15a83e87f6ebcbd0b09247036bc66424b28b370
VLong <vanlongtran2023@gmail.com>
2024-06-13 14:10:48 +0700

    Add return url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-'use client'
+"use client";
 import { TonConnectButton, TonConnectUIProvider } from "@tonconnect/ui-react";
 
 export default function Home() {
   return (
-    <TonConnectUIProvider manifestUrl="https://ton.vote/tonconnect-manifest.json">
+    <TonConnectUIProvider
+      manifestUrl="https://ton.vote/tonconnect-manifest.json"
+      actionsConfiguration={{
+        returnStrategy: "https://t.me/miniappdemobot/demo",
+        twaReturnUrl: "https://t.me/miniappdemobot/demo",
+      }}
+    >
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <TonConnectButton />
       </main>
